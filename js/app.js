314dd836eb40e18ec992fb1e6593f12b4cb03d35
dev_nayan <md.nayan8494@gmail.com>
2024-04-29 01:46:40 +0600

    complete all task

--- js/app.js
+++ js/app.js
@@ -9,6 +9,18 @@ tailwind.config = {
   },
 };
 
+window.addEventListener("scroll", function () {
+  let scroll = window.scrollY;
+  if (scroll > 50) {
+    document.querySelector("#header").style.boxShadow =
+      "0 20px 25px -5px rgb(0 0 0 / 0.1), 0 8px 10px -6px rgb(0 0 0 / 0.1)";
+    document.querySelector("#header").style.background = "rgb(31 41 55)";
+  } else {
+    document.querySelector("#header").style.boxShadow = "none";
+    document.querySelector("#header").style.background = "transparent";
+  }
+});
+
 const menuBtn = document.getElementById("menuBtn");
 const menu = document.getElementById("menu");
 const menuBtnIcon = document.getElementById("menuBtnIcon");
